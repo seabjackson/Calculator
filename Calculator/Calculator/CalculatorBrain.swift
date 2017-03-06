9f47e94627175db318bc94783362998ca998fb0c
Seab <honeyboyseab@gmail.com>
2017-03-05 22:52:36 -0500

    changed the internal structure of the calculator app by using a dictionary of operations. If the user asks to perform an operation we look it up in the dictionary and perform that operation using a closure

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -6,27 +6,76 @@ struct CalculatorBrain {
     
     private var accumulator: Double?
     
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "sin": Operation.unaryOperation(sin),
+        "±": Operation.unaryOperation({ -$0 }),
+        "×": Operation.binaryOperation({ $0 * $1 }),
+        "−": Operation.binaryOperation({ $0 - $1 }),
+        "÷": Operation.binaryOperation({ $0 / $1 }),
+        "+": Operation.binaryOperation({ $0 + $1 }),
+        "=": Operation.equals
+    ]
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    private var pendngBinaryOperation: PendingBinaryOperation?
+    
+    
+    
+    
     var result: Double? {
         get {
             return accumulator
         }
     }
     
+    private mutating func performPendingBinaryOperation() {
+        if pendngBinaryOperation != nil && accumulator != nil {
+            accumulator = pendngBinaryOperation?.perform(with: accumulator!)
+            pendngBinaryOperation = nil
+        }
+        
+    }
+    
     mutating func setOperand(_ operand: Double) {
         accumulator =  operand
     }
     
-    func performOperation(_ symbol: String) {
-        switch mathematicalSymbol {
-        case "π":
-            displayValue = Double.pi
-        case "√":
-            displayValue = sqrt(displayValue)
-        default:
-            break
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendngBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            }
         }
-
     }
-    
 }
-
